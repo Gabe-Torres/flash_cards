6dfe9eae518409949ef7139ed959645652a4309d
Gabriel Torres <thomastgtorres21@gmail.com>
2023-05-10 16:09:12 -0500

    Added card class and test file

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,10 +1,27 @@
 class Turn
-    def initialize(string, card)
-        @string = string 
-        @card = card
+
+    def initialize(guess, card)
         @guess = guess
+        @card = card
     end
+    
     def guess
-        guess
+        @guess
+    end
+
+    def card
+        @card
+    end
+
+    def correct?
+        guess == card.answer
+    end
+
+    def feedback
+        if guess == card.answer
+            "Correct!"
+        else
+            "Incorrect."
+        end
     end
 end
