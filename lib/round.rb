dbdedfcb74d7d6e9bf1f3b3db42822887f105973
Gabriel Torres <thomastgtorres21@gmail.com>
2023-05-11 13:56:19 -0500

    Made changes to Round

--- lib/round.rb
+++ lib/round.rb
@@ -1,3 +1,9 @@
 class Round
-    
+    def initialize(deck)
+        @deck = deck 
+    end
+
+    def take_turn
+        
+    end
 end
--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -14,4 +14,15 @@ RSpec.describe Round do
 
         expect(round).to be_instance_of(Round)
     end
+
+    it 'has a take turn method' do
+        card_1= Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+        card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+        deck = Deck.new([card_1, card_2, card_3])
+        round = Round.new(deck) 
+        turns = []
+
+        expect(round.take_turn).to be_instance_of(Round)
+    end
 end
