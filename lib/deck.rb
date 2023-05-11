8c7ecaa7a42ad496ba8aa39d882a20b24fce1dfa
Gabriel Torres <thomastgtorres21@gmail.com>
2023-05-10 23:28:06 -0500

    Added deck class and testing file

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,5 +1,27 @@
 class Deck
-    def initialize(card)
-        @card = []
+
+    def initialize(card_array)
+        @card_array = card_array
+    end 
+
+    def cards
+        @card_array
+    end
+
+    def count 
+        @card_array.length 
+    end 
+    
+    def cards_in_category(category)
+        #create new array
+        filtered_cards = []
+        @card_array.each do |card|
+            #check card category against category argument
+            if card.category == category 
+            filtered_cards << card
+            end
+        end
+        #telling my code what to return 
+        filtered_cards
     end
 end
